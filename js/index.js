9da74d0b6896b5886f142be02195c865215cabee
SofiiCode <sofi.andand06@gmail.com>
2023-02-14 20:09:37 +0200

    added cards

---
+++ js/index.js
@@ -0,0 +1,44 @@
+let cart = {
+  djdjd: 2,
+  djdjd2: 3,
+};
+
+
+const addFunction = (id) => {
+    console.log(id)
+};
+
+
+document.onclick = (event) => {
+    if (event.target.classList.contains("add")) {
+    addFunction(event.target.dataset.id);
+    }
+  if (event.target.classList.contains("plus")) {
+    plusFunction(event.target.dataset.id);
+  }
+  if (event.target.classList.contains("minus")) {
+    minusFunction(event.target.dataset.id);
+  }
+   
+};
+const plusFunction = (id) => {
+  cart[id]++;
+  renderCart();
+};
+const minusFunction = (id) => {
+  if (cart[id] - 1 == 0) {
+    deleteFunction(id);
+    return true;
+  } else {
+    cart[id]--;
+    renderCart();
+  }
+};
+const deleteFunction = (id) => {
+  delete cart[id];
+  renderCart();
+};
+const renderCart = () => {
+  console.log(cart);
+};
+// renderCart();
