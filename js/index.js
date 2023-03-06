34d5c62b0768953b25fca5ef5d03b29cbe3ae0c7
SofiiCode <sofi.andand06@gmail.com>
2023-03-06 08:30:26 +0200

    added carts

--- js/index.js
+++ js/index.js
@@ -1,12 +1,74 @@
 let cart = {
-  djdjd: 1,
-  djdjd2: 2,
-  djdjd3: 3,
+  
 };
+
+const carts = [
+  { id: "djdjd", product: "Shampoo", numbers: 1, price: 10 },
+  { id: "djdjd2", product: "Souer", numbers: 2, price: 8 },
+  { id: "djdjd3", product: "Cream", numbers: 3, price: 11 },
+];
+console.log(carts)
+for(i=0; i<carts.length; i++){
+  addCarts(carts[i]);
+  
+}
+function addCarts(data){
+  let cartsWrapper = document.getElementById('carts-wrapper');
+ let cartElement = document.createElement("div");
+ let lastDiv = cartsWrapper.lastChild;
+
+ let insertedElement = cartsWrapper.insertBefore(cartElement, lastDiv);
+   insertedElement.classList.add("col-sm-4");
+   insertedElement.classList.add("cart-box");
+
+let cartsHTML = `
+            <img class="img-fluid mt-3" src="https://cdn.shopify.com/s/files/1/0284/6546/3349/products/MetalPegs3_360x.jpg?v=1599600281" alt="Product name">
+            <h5 class="mt-2 cart-tittle">${data.product}</h5>
+            <p class="mt-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores earum eum sed aliquid ipsum dolor. At sequi recusandae iure ab esse temporibus maxime minima modi repudiandae dicta, veniam accusantium asperiores!</p>
+            <p>${data.price}$</p>
+            <button href="#" class="btn btn-second mb-3 add" data-id ="${data.id}">Add to Cart</button>`;
+       
+            insertedElement.innerHTML = cartsHTML;
+}
+
+
+
+
+
+
+
+
 
 
 const addFunction = (id) => {
-    console.log(id)
+    cart[id]=1;
+    let numbers= cart[id]
+    console.log(cart)
+    for (i = 0; i < carts.length; i++) {
+     
+      if(carts[i].id===id){
+         console.log(carts[i].product);
+          let wrapper = document.getElementById("busket-wrapper");
+
+          let cartElement = document.createElement("div");
+          let lastDiv = wrapper.lastChild;
+
+          let insertedElement = wrapper.insertBefore(cartElement, lastDiv);
+          insertedElement.classList.add("col-4");
+          insertedElement.classList.add("cart-box");
+
+          let cartsHTML = `
+            <img class="img-fluid mt-3" src="https://cdn.shopify.com/s/files/1/0284/6546/3349/products/MetalPegs3_360x.jpg?v=1599600281" alt="Product name">
+            <h5 class="mt-2 cart-tittle">${carts[i].product}</h5>
+            <p>${carts[i].price}</p>
+            <p id='num'>${numbers}</p>
+            <button href="" class="btn btn-second mb-3 plus" data-id ="${id}">+</button>
+            <button href="" class="btn btn-second mb-3 minus" data-id ="${id}">-</button>`;
+
+          insertedElement.innerHTML = cartsHTML;
+      }
+    }
+   
 };
 
 
@@ -24,7 +86,7 @@ document.onclick = (event) => {
 };
 const plusFunction = (id) => {
   cart[id]++;
-  renderCart();
+  renderCart(id);
 };
 const minusFunction = (id) => {
   if (cart[id] - 1 == 0) {
@@ -32,14 +94,15 @@ const minusFunction = (id) => {
     return true;
   } else {
     cart[id]--;
-    renderCart();
+    renderCart(id);
   }
 };
 const deleteFunction = (id) => {
   delete cart[id];
-  renderCart();
+  renderCart(id);
 };
-const renderCart = () => {
-  console.log(cart);
+const renderCart = (id) => {
+  let num = document.getElementById('num')
+  num.innerHTML= cart[id]
 };
 // renderCart();
