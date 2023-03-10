aef0a7f4009cf149e5f4f09f6a7c5f189d893712
SofiiCode <sofi.andand06@gmail.com>
2023-03-10 17:49:46 +0200

    plus minus functionality

--- js/index.js
+++ js/index.js
@@ -42,8 +42,8 @@ let cartsHTML = `
 
 const addFunction = (id) => {
     cart[id]=1;
-    let numbers= cart[id]
-    console.log(cart)
+    let number= cart[id]
+  
     for (i = 0; i < carts.length; i++) {
      
       if(carts[i].id===id){
@@ -60,8 +60,9 @@ const addFunction = (id) => {
           let cartsHTML = `
             <img class="img-fluid mt-3" src="https://cdn.shopify.com/s/files/1/0284/6546/3349/products/MetalPegs3_360x.jpg?v=1599600281" alt="Product name">
             <h5 class="mt-2 cart-tittle">${carts[i].product}</h5>
-            <p>${carts[i].price}</p>
-            <p id='num'>${numbers}</p>
+            <span id="price">${carts[i].price}</span><span>$/p</span>
+            <p id='num'>${number}</p>
+            <p><span id="sum">${carts[i].price}</span><span>$</span></p>
             <button href="" class="btn btn-second mb-3 plus" data-id ="${id}">+</button>
             <button href="" class="btn btn-second mb-3 minus" data-id ="${id}">-</button>`;
 
@@ -78,15 +79,23 @@ document.onclick = (event) => {
     }
   if (event.target.classList.contains("plus")) {
     plusFunction(event.target.dataset.id);
+    event.target.parentElement.remove();
+    
   }
   if (event.target.classList.contains("minus")) {
     minusFunction(event.target.dataset.id);
+    event.target.parentElement.remove();
+
   }
    
 };
 const plusFunction = (id) => {
   cart[id]++;
-  renderCart(id);
+  // if (carts[i].id === id){
+    
+  // }
+    // addFunction(id)
+    renderCart(id);
 };
 const minusFunction = (id) => {
   if (cart[id] - 1 == 0) {
@@ -99,10 +108,45 @@ const minusFunction = (id) => {
 };
 const deleteFunction = (id) => {
   delete cart[id];
-  renderCart(id);
+  
 };
 const renderCart = (id) => {
-  let num = document.getElementById('num')
-  num.innerHTML= cart[id]
+
+  let number = cart[id];
+
+  for (i = 0; i < carts.length; i++) {
+    if (carts[i].id === id) {
+      console.log(carts[i].product);
+      let wrapper = document.getElementById("busket-wrapper");
+
+      let cartElement = document.createElement("div");
+      let lastDiv = wrapper.lastChild;
+
+      let insertedElement = wrapper.insertBefore(cartElement, lastDiv);
+      insertedElement.classList.add("col-4");
+      insertedElement.classList.add("cart-box");
+
+      let cartsHTML = `
+            <img class="img-fluid mt-3" src="https://cdn.shopify.com/s/files/1/0284/6546/3349/products/MetalPegs3_360x.jpg?v=1599600281" alt="Product name">
+            <h5 class="mt-2 cart-tittle">${carts[i].product}</h5>
+            <span id="price">${carts[i].price}</span><span>$/p</span>
+            <p id='num'>${number}</p>
+            <p><span id="sum">${carts[i].price}</span><span>$</span></p>
+            <button href="" class="btn btn-second mb-3 plus" data-id ="${id}">+</button>
+            <button href="" class="btn btn-second mb-3 minus" data-id ="${id}">-</button>`;
+
+      insertedElement.innerHTML = cartsHTML;
+    }
+  }
+
+
+  // let num = document.getElementById('num')
+  // num.innerHTML= cart[id]
+    let price = document.getElementById("price").innerHTML;
+    let sumHTML = document.getElementById("sum");
+    console.log(price);
+    sumHTML.innerHTML = price * cart[id];
+
+   
 };
 // renderCart();
